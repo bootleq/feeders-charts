2342e9e5592d90c3a290bf752eb23c988f713e01
bootleq <bootleq@gmail.com>
2025-01-30 14:58:30 +0800

    Fix basePath for development mode
    Only take custom basePath in production mode (like in build).
    
    During development, just assume directly using node dev server, without
    any basePath.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === 'development';
+
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  basePath: isDev ? '' : process.env.NEXT_PUBLIC_BASE_PATH,
   transpilePackages: ["echarts", "zrender"],
 };
 
