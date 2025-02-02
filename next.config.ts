03a84d79dec81e7ab6956ed255df69fab8282aae
bootleq <bootleq@gmail.com>
2025-02-02 16:10:21 +0800

    Config SVGR in next.config
    Have to use different config for turbo (development) and webpack
    (production).

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import type { webpack } from 'next/dist/compiled/webpack/webpack'
 
 const isDev = process.env.NODE_ENV === 'development';
 
@@ -6,6 +7,37 @@ const nextConfig: NextConfig = {
   output: 'export',
   basePath: isDev ? '' : process.env.NEXT_PUBLIC_BASE_PATH,
   transpilePackages: ["echarts", "zrender"],
+
+  experimental: {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
+
+  webpack(config) {
+    const fileLoaderRule = config.module.rules.find((rule: webpack.RuleSetUseItem) => rule.test?.test?.(".svg"));
+
+    config.module.rules = [
+      ...config.module.rules,
+      {
+        ...fileLoaderRule,
+        test: /icon.svg$/,
+      },
+      {
+        test: /\.svg$/,
+        exclude: /icon.svg$/,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+    ];
+
+    return config;
+  },
 };
 
 export default nextConfig;
