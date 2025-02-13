65820aa7442b2f64ef5a6a8fb2b7ae81fdf57d45
bootleq <bootleq@gmail.com>
2025-02-13 10:01:02 +0800

    Move tooltipClass functions to utils module
    Previously these functions were exported in `chart.tsx`

--- src/app/dog-populations/chart.tsx
+++ src/app/dog-populations/chart.tsx
@@ -15,6 +15,7 @@ import { YearsInput } from './YearsInput';
 import { SeriesControl } from './SeriesControl';
 import { RepresentControl } from './RepresentControl';
 import { MarkerControl } from './MarkerControl';
+import { tooltipClass } from './utils';
 
 import type { CheckboxSet } from './store';
 import { defaultOptions, defaultSeriesSettings } from './defaults';
@@ -57,17 +58,6 @@ echarts.use(
     CanvasRenderer,
   ]
 );
-
-export function tooltipClass(className?: string) {
-  return `rounded box-border w-max z-[1002] bg-slate-100 ${className || ''}`;
-}
-export function tooltipMenuCls(className?: string) {
-  return [
-    'flex flex-col divide-y w-full items-center justify-between',
-    'rounded bg-gradient-to-br from-stone-50 to-slate-100 ring-2 ring-offset-1 ring-slate-300',
-    className || '',
-  ].join(' ');
-}
 
 export default function Chart({ items, meta }: {
   items: CountryItem[],
