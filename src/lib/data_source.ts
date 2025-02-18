a8a7b4cf023ca1ae55858880dbb69e6d71725abc
bootleq <bootleq@gmail.com>
2025-02-18 11:28:06 +0800

    Move some source definition to new lib/resource
    Previous `lib/data_source` is not meant to be used by web app, but only
    for development building process.
    
    Now move the really public parts to new `lib/resource` module, prepare
    to use them later.

--- src/lib/data_source.ts
+++ src/lib/data_source.ts
@@ -3,37 +3,9 @@ import fs from "fs";
 import fsp from 'node:fs/promises';
 import path from "path";
 import crypto from 'crypto';
+import { shelter_reports_table, jsonMetaReviver } from '@/lib/resource';
 
 export const DATA_DIR = 'data';
-
-const jsonMetaReviver = (key: string, value: any) => {
-  if (key === 'sourceCheckedAt' && typeof value === 'string') {
-    return new Date(value);
-  }
-
-  return value;
-};
-
-const shelter_reports_table = [
-  // NOTE: 107 年之後改用「動物收容統計表（詳表）」資料，這邊不採用
-  // [113, '113年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/250117114614324698T87CD.xlsx'],
-  // [112, '112年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2402191131342031173DCLA.xlsx'],
-  // [111, '111年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2304270146565611513V5GC2JATE.xlsx'],
-  // [110, '110年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/220315063424098101XYJV5.xls'],
-  // [109,   '109年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/210513043948020470N5CB8.xls'],
-  // [108,   '108年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200215115223422925LT732.ods'],
-  [107,   '107年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/download/resources/A20190100004_exl.xlsx'],
-  [106,   '106年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/download/resources/A20170700007_exl.xlsx'],
-  [105,   '105年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/1912170324497935458R4FW.xlsx'],
-  [104,   '104年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/191217032334835687JFPVG.xlsx'],
-  [103,   '103年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/1912170322142536496NFME.xlsx'],
-  [102,   '102年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/191219102621856549UFLL6.xlsx'],
-  [101,   '101年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102112119455694TXAHC.xlsx'],
-  [100,   '100年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102112001361784JQJW1.xlsx'],
-  [ 99,    '99年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2001021116496582852GF9V.xlsx'],
-  [ 98,    '98年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102111608251945H65FM.xlsx'],
-  [ 97,    '97年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2001021115231581118SZFC.xlsx'],
-] as const;
 
 const shelter_reports = shelter_reports_table.reduce((acc, [year, title, url]) => {
   const name = `shelter_reports_${year}`;
@@ -61,6 +33,7 @@ type Sources = Record<string, {
 }>;
 
 // NOTE: Not all data were defined in `sources`, see README for full resource list.
+// FIXME: avoid confusion with @/lib/resource
 export const sources: Sources = {
   population: {
     title: '年度犬貓統計表',
---
+++ src/lib/resource.ts
@@ -0,0 +1,89 @@
+export const jsonMetaReviver = (key: string, value: any) => {
+  if (['sourceCheckedAt', 'builtAt'].includes(key) && typeof value === 'string') {
+    return new Date(value);
+  }
+
+  return value;
+};
+
+const moaGovShelterLink = 'https://animal.moa.gov.tw/Frontend/Know/PageTabList?TabID=31B05CB46007226417F0F5FB8A80096E#tab3';
+
+export const shelter_reports_table = [
+  // NOTE: 107 年之後改用「動物收容統計表（詳表）」資料，這邊不採用
+  // [113, '113年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/250117114614324698T87CD.xlsx'],
+  // [112, '112年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2402191131342031173DCLA.xlsx'],
+  // [111, '111年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2304270146565611513V5GC2JATE.xlsx'],
+  // [110, '110年度全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/220315063424098101XYJV5.xls'],
+  // [109,   '109年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/210513043948020470N5CB8.xls'],
+  // [108,   '108年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200215115223422925LT732.ods'],
+  [107,   '107年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/download/resources/A20190100004_exl.xlsx'],
+  [106,   '106年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/download/resources/A20170700007_exl.xlsx'],
+  [105,   '105年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/1912170324497935458R4FW.xlsx'],
+  [104,   '104年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/191217032334835687JFPVG.xlsx'],
+  [103,   '103年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/1912170322142536496NFME.xlsx'],
+  [102,   '102年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/191219102621856549UFLL6.xlsx'],
+  [101,   '101年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102112119455694TXAHC.xlsx'],
+  [100,   '100年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102112001361784JQJW1.xlsx'],
+  [ 99,    '99年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2001021116496582852GF9V.xlsx'],
+  [ 98,    '98年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/200102111608251945H65FM.xlsx'],
+  [ 97,    '97年全國公立動物收容所收容處理情形統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/2001021115231581118SZFC.xlsx'],
+] as const;
+
+const shelter_reports_resources = shelter_reports_table.reduce((acc, [year, title]) => {
+  const name = `shelter_reports_${year}`;
+  return {
+    ...acc,
+    ...{
+      [name]: {
+        title,
+        docUrl: moaGovShelterLink,
+      },
+    }};
+}, {});
+
+type ResourceEntry = {
+  title: string,
+  docUrl?: string
+};
+
+export const resources: Record<string, ResourceEntry> = {
+  population: {
+    title: '年度犬貓統計表',
+    docUrl: 'https://data.gov.tw/dataset/41771',
+  },
+  human_population: {
+    title: '戶籍登記人口數(人)',
+    docUrl: 'https://winstacity.dgbas.gov.tw/DgbasWeb/ZWeb/StateFile_ZWeb.aspx',
+  },
+  shelter_pet: {
+    title: '動物收容統計表（詳表）（108 ~ 113 年）',
+    docUrl: 'https://www.pet.gov.tw/AnimalApp/ReportAnimalsAcceptFront.aspx',
+  },
+  populations_112: {
+    title: '112年度全國家犬貓數量調查結果統計表',
+    docUrl: 'https://animal.moa.gov.tw/Frontend/Know/Detail/LT00000817?parentID=Tab0000143',
+  },
+  populations_113: {
+    title: '113年各縣市遊蕩犬估計數調查結果',
+    docUrl: 'https://animal.moa.gov.tw/Frontend/Know/Detail/LT00000864?parentID=Tab0000143',
+  },
+  countrywide: {
+    title: '民國 96 年以前的資料',
+    docUrl: 'https://docs.google.com/spreadsheets/d/1ajrN-ok3wnSI8X2-ntRRX9W8B2rXIz5ScnZWyUzt-G4/edit?gid=0#gid=0',
+  },
+  heat_map: {
+    title: '遊蕩犬熱區圖 (2023 ~ 2024)',
+    docUrl: 'https://www.pet.gov.tw/Wandering/HeatMapV1.aspx',
+  },
+  combined: {
+    title: '整理後的資料',
+  },
+  ...shelter_reports_resources,
+} as const;
+
+export function resourceInfo(resourceName: string) {
+  const found = resources[resourceName];
+  if (found) {
+    return found;
+  }
+}
