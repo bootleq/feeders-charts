9a58bd711460972d76ef2560d3395337e669d8b3
bootleq <bootleq@gmail.com>
2025-02-21 22:03:00 +0800

    Fix turbopack is used in building
    Seems whenever `turbo` is appears in next config, the process will bring
    turbo up.
    
    Now detect environment and only inject turbo config for development.

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,12 @@
 import type { NextConfig } from "next";
 import type { webpack } from 'next/dist/compiled/webpack/webpack'
 
+const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
   output: 'export',
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   transpilePackages: ["echarts", "zrender"],
-
-  experimental: {
-    turbo: {
-      rules: {
-        '*.svg': {
-          loaders: ['@svgr/webpack'],
-          as: '*.js',
-        },
-      },
-    },
-  },
 
   webpack(config) {
     const fileLoaderRule = config.module.rules.find((rule: webpack.RuleSetUseItem) => rule.test?.test?.(".svg"));
@@ -38,5 +28,18 @@ const nextConfig: NextConfig = {
     return config;
   },
 };
+
+if (isDev) {
+  nextConfig['experimental'] = {
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  };
+}
 
 export default nextConfig;
