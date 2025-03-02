bootleq/feeders-charts

commit ea34e07a356ec7ae30c5c19eab29b3d6fb236025

Add data source for workforce (動物保護業務人力)
各縣市動物保護業務人力
https://animal.moa.gov.tw/Frontend/Know/PageTabList?TabID=31B05CB46007226417F0F5FB8A80096E#tab4

- Those PDFs are simple and easier to parse, so don't keep raw source
  files (i.e., for debugging purpose), just process all files and saves
  one combined file.

- Extend `cityLookup` to allow mismatch without raising error.

- Introduce new `blank` function in lib/utils, and since using it in
  data scripts, allow `APP_URL` to omit absence of env setting, which
  might be empty when running data script.

src/lib/model.ts
@@ -119,7 +119,7 @@ export const LEGACY_CITY_MAPPING = {
 
 const revertCityMapping = R.invertObj(CITY_MAPPING);
 
-export function cityLookup(nameOrCode: string) {
+export function cityLookup(nameOrCode: string, silent?: boolean) {
   if (R.has(nameOrCode, CITY_MAPPING)) {
     return CITY_MAPPING[nameOrCode];
   }
@@ -127,6 +127,8 @@ export function cityLookup(nameOrCode: string) {
   if (R.has(nameOrCode, revertCityMapping)) {
     return revertCityMapping[nameOrCode];
   }
+
+  if (silent) return undefined;
 
   throw new Error(`Unknown city lookup: ${nameOrCode}`);
 }

scripts/prepare_workforce.mjs
@@ -1,0 +1,213 @@
+import * as R from 'ramda';
+import fsp from 'node:fs/promises';
+import fetch from 'node-fetch';
+import pdfTableExtractor from 'pdf-table-extractor';
+import chalk from 'chalk';
+import { cityLookup } from '@/lib/model';
+import { blank } from '@/lib/utils';
+import { workForceTable } from '@/lib/resource';
+import { testSamplesExist } from './utils';
+import {
+  downloadPath,
+  buildingPath,
+  checkUpdateHash,
+  writeSourceTime,
+} from './data_source';
+
+const resourceName = 'workforce';
+
+async function extractPDFTable(file) {
+  return new Promise((resolve, reject) => {
+    pdfTableExtractor(
+      file,
+      (result) => resolve(result),
+      (error) => reject(error),
+    );
+  });
+}
+
+function toNumber(str) {
+  return Number(str.trim().replaceAll(',', ''));
+}
+
+function validate(data) {
+  const samples = [
+    {
+      year: 110,
+      city: 'City000006',
+      ft_shelter: 7,
+      pt_shelter: 3,
+      ft_vet: 1,
+      pt_vet: 3,
+      ft_inspct: 2,
+      pt_inspct: 0,
+    }, // 新竹市 110
+    {
+      year: 111,
+      city: 'City000022',
+      ft_shelter: 3,
+      pt_shelter: 3,
+      ft_vet: 1,
+      pt_vet: 1,
+      ft_inspct: 2,
+      pt_inspct: 0,
+    }, // 連江縣 111
+    {
+      year: 112,
+      city: 'City000017',
+      ft_shelter: 6,
+      pt_shelter: 4,
+      ft_vet: 1,
+      pt_vet: 2,
+      ft_inspct: 5,
+      pt_inspct: 4
+    }, // 臺東縣 112
+    {
+      year: 113,
+      city: 'City000005',
+      ft_shelter: 6,
+      pt_shelter: 1,
+      ft_vet: 1,
+      pt_vet: 2,
+      ft_inspct: 6,
+      pt_inspct: 2,
+      ft_etc: 9,
+      pt_etc: 2,
+    }, // 新竹縣 113
+  ];
+
+  return testSamplesExist(samples, data);
+}
+
+function parseTable(pageTables, year) {
+  let data = [];
+
+  for (const page of pageTables) {
+    const { tables: rows } = page;
+    const [header1, header2, ...bodyRows] = rows;
+
+    if (year >= 113) {
+      if (
+        header1[1] !== '收容所管理人員' ||
+        R.not(/收容處所獸醫師$/.test(header1[3].replace(/\n/, ''))) ||
+        header1[5] !== '動物保護檢查員' ||
+        R.not(/其他動物保護業務/.test(header1[7].replace(/\n/, '')))
+      ) {
+        throw new Error(`Unexpected table header1: ${JSON.stringify(header1)}`);
+      }
+    } else {
+      if (JSON.stringify(header1) !== JSON.stringify(['縣市別', '收容所管理人員', '',  '駐場獸醫師', '', '動物保護檢查員', ''])) {
+        throw new Error(`Unexpected table header1: ${JSON.stringify(header1)}`);
+      }
+    }
+
+    if (JSON.stringify(header2.slice(0, 7)) !== JSON.stringify(['', '專職', '兼職', '專職', '兼職', '專職', '兼職'])) {
+      throw new Error(`Unexpected table header2: ${JSON.stringify(header2)}`);
+    }
+
+    bodyRows.forEach((row) => {
+      const [cityName, ...values] = row;
+      const city = cityLookup(cityName, true);
+      if (blank(city)) return;
+
+      data.push({
+        city: city,
+        ...R.zipObj([
+          'ft_shelter',
+          'pt_shelter',
+          'ft_vet',
+          'pt_vet',
+          'ft_inspct',
+          'pt_inspct',
+          'ft_etc',
+          'pt_etc',
+        ], values.map(toNumber))
+      });
+    });
+  }
+
+  return data;
+}
+
+async function parsePDF(file, year) {
+  let data = [];
+  const { pageTables } = await extractPDFTable(file);
+
+  data = parseTable(pageTables, year);
+  return data.map(obj => ({ year, ...obj }));
+}
+
+async function fetchData(url) {
+  const response = await fetch(url);
+  if (!response.ok) throw new Error(`Failed to fetch ${url}: ${response.statusText}`);
+  return response.arrayBuffer();
+}
+
+async function download(subResourceName, url, pdfPath) {
+  console.log(`Download data for ${subResourceName} ...`);
+
+  const remoteData = await fetchData(url);
+  const buffer = Buffer.from(remoteData);
+  await fsp.writeFile(pdfPath, buffer);
+}
+
+(async function main() {
+  console.log("Prepare workforce data...\n");
+
+  let data = [];
+
+  for (const [year,, url] of workForceTable) {
+    const basename = `${resourceName}_${year}`;
+    const pdfPath = downloadPath(basename, `raw.pdf`);
+    await download(basename, url, pdfPath);
+
+    let seasonData = await parsePDF(pdfPath, year);
+    data = [...data, ...seasonData];
+  }
+
+  data = R.pipe(
+    R.groupBy(R.props(['city', 'year'])),
+    R.values,
+    R.map(
+      R.reduce((acc, obj) =>
+        R.mergeWithKey(
+          (key, a, b) => {
+            if (['city', 'year'].includes(key)) {
+              return a;
+            } else {
+              return (R.is(Number, a) && R.is(Number, b) ? a + b : a);
+            }
+          },
+          acc,
+          obj
+        ),
+        {}
+      ))
+    )(data);
+
+  if (!validate(data)) {
+    console.error('Aborted, validation failed.');
+    return;
+  }
+
+  const newContent = JSON.stringify(data, null, 2);
+  const needsUpdate = await checkUpdateHash(resourceName, newContent);
+
+  if (needsUpdate) {
+    await writeSourceTime(resourceName);
+  } else {
+    console.log(`Source data for '${resourceName}' has no change.`);
+
+    if (Number(process.env.DATA_CONTINUE_WHEN_SAME_HASH)) {
+      console.log(chalk.yellow.bold('but still process per request.') + chalk.red.bold('(DATA_CONTINUE_WHEN_SAME_HASH)'));
+    } else {
+      return;
+    }
+  }
+
+  const outFile = buildingPath(resourceName, 'json');
+  await fsp.writeFile(outFile, newContent);
+  console.log(`Successfully wrote file to ${outFile}\n`);
+
+  console.log("\nDone.");
+})();

scripts/reduce_data.mjs
@@ -26,6 +26,7 @@ const manuallyResources = [
   'heat_map',
   'shelter_pet',
   'law_enforce',
+  'workforce',
 ];
 
 async function normalize( resourceName ) {

src/lib/resource.ts
@@ -61,6 +61,15 @@ const lawEnforceTableResources = lawEnforceTable.reduce((acc, [year, title]) => 
     }};
 }, {});
 
+const moaWorkforceLink = 'https://animal.moa.gov.tw/Frontend/Know/PageTabList?TabID=31B05CB46007226417F0F5FB8A80096E#tab4';
+
+export const workForceTable = [
+  [110, '110年4月各縣市動物保護業務人力統計表',  'https://animal.moa.gov.tw/public/upload/Know_ListFile/220511094438712076M2B9T.pdf'],
+  [111, '111年10月各縣市動物保護業務人力統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/221229103502641266SBXNC.pdf'],
+  [112, '112年10月各縣市動物保護業務人力統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/231208043131215611YEHKE.pdf'],
+  [113, '113年10月各縣市動物保護業務人力統計表', 'https://animal.moa.gov.tw/public/upload/Know_ListFile/241129061152849406ELCCZ.pdf'],
+];
+
 export type ResourceEntry = {
   title: string,
   docUrl?: string
@@ -103,5 +112,9 @@ export const resources: Record<string, ResourceEntry> = {
   law_enforce: {
     title: '各縣市政府執行動物保護法案件情形（合併）',
     docUrl: moaGovEnforcementLink,
+  },
+  workforce: {
+    title: '各縣市動物保護業務人力',
+    docUrl: moaWorkforceLink,
   },
 } as const;

src/lib/utils.ts
@@ -2,11 +2,14 @@ import * as R from 'ramda';
 import { format } from 'date-fns';
 import { zhTW } from 'date-fns/locale';
 
+const appURLCfg = process.env.NEXT_PUBLIC_APP_URL;
+
 export const SITE_NAME = 'Feeders（暫名）';
-export const APP_URL = new URL(process.env.NEXT_PUBLIC_APP_URL!);
+export const APP_URL = appURLCfg && new URL(appURLCfg);
 export const BASE_PATH = process.env.NEXT_PUBLIC_BASE_PATH || '';
 
 export const present = R.both(R.isNotNil, R.isNotEmpty);
+export const blank = R.complement(present);
 
 export function makeYearRange(min: number, max: number) {
   return Array.from(

commit 6038745592bae24308aa1baa0c70aca41722f5c3

Various TypeScript fixes
First 2 issues were introduced by ea34e07:

- Ensure `APP_URL` present when app running.
  Check it in main layout, but still allow absence in script.

- Overload `cityLookup` function to ensure no `undefined` will appear
  when `silent` argument is absent.

- Explicitly declare tooltip `placement` is of `Placement` type,
  otherwise assuming String is too loosen. Problem introduced b6405e9.

src/app/layout.tsx
@@ -6,6 +6,10 @@ import ProgressBar from './ProgressBar';
 import { SITE_NAME, APP_URL, present } from '@/lib/utils';
 import "./globals.css";
 
+if (!APP_URL) {
+  throw new Error('Missing APP_URL.');
+}
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],

src/components/MenuDescTooltip.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import { Tooltip, TooltipTrigger, TooltipContent, menuHoverProps } from '@/components/Tooltip';
+import type { Placement } from '@floating-ui/react';
 
 // 遊蕩犬
 const tipRoaming = (<>
@@ -230,7 +231,7 @@ const dict: Record<string, React.JSX.Element|string> = {
 };
 
 export const MenuDescTooltip = ({ name, children }: { name: string|undefined, children: React.ReactNode }) => {
-  let key, alt, placement = 'bottom';
+  let key, alt, placement: Placement = 'bottom';
 
   if (name?.startsWith('ft_') || name?.startsWith('pt_')) {
     [alt, key] = name ? name.split('_', 2) : [,];

src/lib/model.ts
@@ -121,7 +121,10 @@ export const LEGACY_CITY_MAPPING = {
 
 const revertCityMapping = R.invertObj(CITY_MAPPING);
 
-export function cityLookup(nameOrCode: string, silent?: boolean) {
+export function cityLookup(nameOrCode: string, silent: true): string | undefined;
+export function cityLookup(nameOrCode: string, silent: false): string;
+export function cityLookup(nameOrCode: string): string;
+export function cityLookup(nameOrCode: string, silent = false) {
   if (R.has(nameOrCode, CITY_MAPPING)) {
     return CITY_MAPPING[nameOrCode];
   }