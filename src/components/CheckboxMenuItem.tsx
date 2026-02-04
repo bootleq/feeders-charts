b8d3d4a817dc8e9ed66ec6a24fde566385007c10
bootleq <bootleq@gmail.com>
2026-02-04 17:59:52 +0800

    Implement MiddleClick Focus on CheckboxMenuItem
    The toggle items of Series/Marker Control floating menus can also be
    benefited by middle-click-focus-one function.
    
    They can't use previously added useListToggleFocus hook because they are
    controlled elements.
    
    Now implement the function within CheckboxMenuItem itself. When
    middle-click, the affected area is the current opened "menu", do not
    disable items in other menus.

--- src/components/CheckboxMenuItem.tsx
+++ src/components/CheckboxMenuItem.tsx
@@ -1,6 +1,6 @@
 import * as R from 'ramda';
-import { useMemo } from 'react';
-import { atom, useAtom, PrimitiveAtom } from 'jotai';
+import { useCallback, useMemo } from 'react';
+import { atom, useAtom, PrimitiveAtom, useSetAtom } from 'jotai';
 import type { CheckboxSet } from '@/components/types';
 import {
   CornerDownRightIcon,
@@ -17,6 +17,20 @@ const checkboxMenuItemAtom = (boxsetAtom: PrimitiveAtom<CheckboxSet>, key: strin
   }
 );
 
+const focusItemAtom = (boxsetAtom: PrimitiveAtom<CheckboxSet>) => atom(
+  null,
+  (get, set, key: string, menuValues: string[]) => {
+    const obj = get(boxsetAtom);
+    obj[key] = true;
+    set(
+      boxsetAtom, R.mapObjIndexed(
+        (v, k) => k === key ? v : (menuValues.includes(k) ? false : v),
+        obj
+      )
+    );
+  }
+);
+
 export function CheckboxMenuItem(boxsetAtom: PrimitiveAtom<CheckboxSet>, inputName: string) {
   return function MenuItem({ Icon, name, iconClass, children, sub, onClick }: {
     Icon?: any,
@@ -29,19 +43,33 @@ export function CheckboxMenuItem(boxsetAtom: PrimitiveAtom<CheckboxSet>, inputNa
     const itemAtom = useMemo(() => checkboxMenuItemAtom(boxsetAtom, name || 'dummy'), [name]);
     const [checked, toggle] = useAtom(itemAtom);
 
+    const focusAtom = useMemo(() => focusItemAtom(boxsetAtom), []);
+    const focus = useSetAtom(focusAtom);
+
+    const onMiddleClick = useCallback((e: React.MouseEvent<HTMLElement>) => {
+      if (e.button !== 1 || !name) return; // "middle" click only
+      const $target = e.target as HTMLElement;
+      const $menu = $target.closest('[role="menu"]');
+      if ($menu) {
+        e.preventDefault();
+        const allValues = Array.from($menu.querySelectorAll('input[type="checkbox"][value]')).map(i => (i as HTMLInputElement).value);
+        focus(name, allValues);
+      }
+    }, [focus, name]);
+
     const menuBtnCls = 'p-2 w-full cursor-pointer flex items-center rounded hover:bg-amber-200';
     const menuIconCls = 'w-[1.25em] h-auto aspect-square box-content';
     const IconElement = Icon || (sub ? CornerDownRightIcon : 'div');
 
     return (
       <MenuDescTooltip name={name}>
-        <label className={menuBtnCls}>
+        <label className={menuBtnCls} onMouseDown={onMiddleClick}>
           <div className='pr-1.5 mr-1 border-r'>
             <IconElement className={`${menuIconCls} ${sub ? 'stroke-slate-400' : ''} ${iconClass || ''}`} />
           </div>
           {
             R.isNil(onClick) ?
-              <input type='checkbox' name={inputName} checked={checked} onChange={toggle} className='peer mb-1 sr-only' /> :
+              <input type='checkbox' name={inputName} checked={checked} onChange={toggle} className='peer mb-1 sr-only' {...(name ? {value: name} : {})} /> :
               <input type='checkbox' onClick={onClick} defaultChecked className='peer mb-1 sr-only' />
           }
           <div className='text-slate-400 outline-blue-400 peer-checked:text-slate-700 peer-focus-visible:outline'>
